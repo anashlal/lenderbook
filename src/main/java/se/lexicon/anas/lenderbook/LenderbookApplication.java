71ed53843436042b0d4f6f16a2b925a1efdc7238
anashlal <50236932+anashlal@users.noreply.github.com>
2020-03-07 23:20:10 +0100

    first commit

---
+++ src/main/java/se/lexicon/anas/lenderbook/LenderbookApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.anas.lenderbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenderbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LenderbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/anas/lenderbook/LenderbookApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.anas.lenderbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenderbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
